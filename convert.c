86a481a991503932fb5d3c8cbbf2d0d80108101e
Anam <anammahmud.eee08@gmail.com>
2018-03-25 02:44:49 -0400

    edited

--- convert.c
+++ convert.c
@@ -1,32 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-char *GetCalRational(char data[1024]){
 
-
-	char input[1024];
-	//input=*data;
+char *GetCalRational(void){
+		char input[1024];
 	int count;
 	count=2;
+
+	File = fopen("data2.csv","r");
+	while(fgets(data,1024,File)){
+
+
 	char *set;
 	//printf("%s\n",data);
 	set=strtok(data,",");
-	while(count--){
-		set=strtok(NULL,",");
-	}
-	return set;
+		while(count--){
+			set=strtok(NULL,",\n");
+		}
+		return set;
 }
-char *GetValue(char data[1024]){
+fclose(File);
+	
+}
+char *GetValue(void){
 	char input[1024];
-	//input=*data;
 	int count;
 	count=1;
+
+File = fopen("data2.csv","r");
+while(fgets(data,1024,File)){
+
+
 	char *set;
 	//printf("%s\n",data);
 	set=strtok(data,",");
 	while(count--){
-		set=strtok(NULL,",");
+		set=strtok(NULL,",\n");
 	}
+}
+fclose(File);
 	return set;
 
 }
@@ -35,7 +47,7 @@ char *GetProperty(char data[1024]){
 	char input[1024];
 	char *set;
 	set=strtok(data,",");
-	set=strtok(NULL,",");
+	//set=strtok(NULL,",");
 	return set;
 
 }
@@ -45,20 +57,31 @@ int main(){
 
 
 FILE *File, *FileV2, *FileV3,*file;
-File = fopen("data1.csv","r");
-file = fopen("data2.csv","w+");
+File = fopen("data2.csv","r");
+//file = fopen("data2.csv","w+");
 char *test, *Test;
 char data[1024];
 
 while(fgets(data,1024,File)){
+		char *set2;
+	set2=GetProperty(data);
+	//printf("-Property\n");
+	printf("Property=%s\n",set2);
+
 
 	char *set;
 	set=GetCalRational(data);
-		printf("-GetCal\n");
-	printf("set=%s\n",set);
+	//printf("-GetCal\n");
+	printf("CAL=%s\n",set);
+
+		//char *set1;
+	set=GetValue(data);
+	//printf("-Value");
+	printf("Value=%s\n",set);
+
 }
 fclose(File);
-File = fopen("data1.csv","r");
+/*File = fopen("data1.csv","r");
 char data2[1024];
 while(fgets(data2,1024,File)){
 	char *set1;
@@ -77,8 +100,8 @@ while(fgets(data3,1024,FileV3)){
 	set2=GetProperty(data3);
 	printf("-Property\n");
 	printf("%s\n",set2);
-}
-fclose(File);
+}*/
+//fclose(File);
 return 0;
 
 
